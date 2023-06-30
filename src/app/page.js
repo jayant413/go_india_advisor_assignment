74238902f7292c5acf7d4a2ec0929a514802cb1e
jayant413 <jayantsawarkar4103@gmail.com>
2023-06-30 15:31:01 +0530

    go inida first commit

---
+++ src/app/page.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="text-blue-600">
+      Go India Advisor
+    </div>
+  )
+}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+const Navbar = ({}) => {
+  return (
+    <div>
+      <h1 className="bg-sky-500 text-orange-400">Navbar</h1>
+    </div>
+  );
+};
+
+export default Navbar;
